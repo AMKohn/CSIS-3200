f31d7c022633125811ee9e3986fed0086889549f
Avi Kohn <AMKohn@users.noreply.github.com>
2019-11-10 15:12:18 -0500

    Updated logger to send real fake request

--- mock/logger.go
+++ mock/logger.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	Conn, _ := net.DialUDP("udp", nil, &net.UDPAddr{ IP: []byte{127, 0, 0, 1}, Port: 1337, Zone: "" })
+	Conn, _ := net.DialUDP("udp", nil, &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 1337, Zone: ""})
 
 	defer Conn.Close()
 
-	Conn.Write([]byte("hello"))
+	Conn.Write([]byte(`{ "type": "web_request", "server_id": "string", "request_type": "GET/POST/...", "url": "", "status_code": 400, "response_time": 1000, "ip_address": "10.0.0.1", "user_agent": "" }`))
 }
