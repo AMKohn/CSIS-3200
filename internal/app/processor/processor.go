29951a47e08f39d38a92554a94e90f13a36c0754
Broganoc <37879767+Broganoc@users.noreply.github.com>
2019-11-10 15:59:32 -0500

    Update processor.go

--- internal/app/processor/processor.go
+++ internal/app/processor/processor.go
@@ -1,6 +1,10 @@
 package processor
 
-import "csis3200/internal/app/dashboard"
+import (
+	"csis3200/internal/app/dashboard"
+	"time"
+)
+
 
 // Have a method to get JSON from the ingester
 // "Save" log message in an array or something
@@ -8,6 +12,13 @@ import "csis3200/internal/app/dashboard"
 
 // This shouldn't be an array, it needs to be something like a queue that only keeps up to 1000 messages, FIFO
 var messagesDb [1000]map[string]interface{}
+
+
+func SaveMessage(message map[string]interface{}){
+	timeStamp := time.Now().UnixNano() / 1000000
+	
+
+}
 
 func HandleMessage(message map[string]interface{}) {
 	// "Save" the message
